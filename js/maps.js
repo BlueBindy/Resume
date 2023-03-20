af66808ef83b92ed2f60681838c508de7419a4d6
BlueBindy <fionajthompson@outlook.com>
2023-03-20 19:13:26 +0000

    Garbage from module

--- js/maps.js
+++ js/maps.js
@@ -26,3 +26,4 @@ function initMap() {
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
+
